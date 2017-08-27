cce812ddcd1c6b8efb425786161c8d8bfd0f13f4
AuFilElec <aufil.elec@gmail.com>
2017-08-27 14:05:33 +0200

    Fix wrong declarations webserver.h

--- src/webserver.h
+++ src/webserver.h
@@ -24,19 +24,11 @@
 
 // Include main project include file
 #include "remora.h"
-#include <AsyncJson.h>
 
 // Exported variables/object instanciated in main sketch
 // =====================================================
 extern char response[];
 extern uint16_t response_idx;
-
-const char FP_SSID[]        PROGMEM = "ssid";
-const char FP_RSSI[]        PROGMEM = "rssi";
-const char FP_ENCRYPTION[]  PROGMEM = "enc";
-const char FP_CHANNEL[]     PROGMEM = "chan";
-const char FP_STATUS[]      PROGMEM = "status";
-const char FP_OK[]          PROGMEM = "OK";
 
 // declared exported function from webserver.cpp
 // ===================================================
@@ -45,8 +37,8 @@ void handleFactoryReset(AsyncWebServerRequest *request);
 void handleReset(AsyncWebServerRequest *request);
 void tinfoJSON(AsyncWebServerRequest *request);
 void tinfoJSONTable(AsyncWebServerRequest *request);
-String sysJSONTable(AsyncWebServerRequest *request);
-String confJSONTable(AsyncWebServerRequest *request);
+void sysJSONTable(AsyncWebServerRequest *request);
+void confJSONTable(AsyncWebServerRequest *request);
 void spiffsJSONTable(AsyncWebServerRequest *request);
 void wifiScanJSON(AsyncWebServerRequest *request);
 void handleNotFound(AsyncWebServerRequest *request);
